4594777cab5c2220ce2fefddb731709ff2367eeb
Kate Connolly <katherine.d.connolly@gmail.com>
2018-11-14 18:07:07 +0000

    updated for knex documentation

--- test_script.js
+++ test_script.js
@@ -1,5 +1,6 @@
 const pg = require("pg");
-const settings = require("./settings"); // settings.json
+const settings = require("./settings");
+
 
 const client = new pg.Client({
     user     : settings.user,
